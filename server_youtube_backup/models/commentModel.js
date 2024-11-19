95d175870aec0a010ba956a4ad9c349f89bf3270
Ziv Olewsky <48315677+zivole@users.noreply.github.com>
2024-11-19 23:22:36 +0200

    Delete server_youtube_backup directory

--- server_youtube_backup/models/commentModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    videoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Video', required: true },
-});
-
-module.exports = mongoose.model('Comment', commentSchema);
--- server_youtube_backup/routes/tokenRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createToken } = require('../controllers/tokenController');
-
-router.post('/', createToken);
-//router.post('/verify', verifyToken);
-
-module.exports = router;
