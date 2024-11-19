e52545b29412d147beb3ed8ad338e76d063b22d9
Ziv Olewsky <48315677+zivole@users.noreply.github.com>
2024-11-19 23:22:51 +0200

    Delete youtube_backup directory

--- youtube_backup/src/views/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../assets/YouTube-Logo.darkmode.svg';
-
-const Logo = ({ className }) => (
-  <img src={logo} alt="Logo" className={`logo ${className}`} />
-);
-
-export default Logo;
